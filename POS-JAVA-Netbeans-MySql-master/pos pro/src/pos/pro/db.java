3fbbc15917d9fa7be2f94354022032eafdf8d270
loddaoceania <murilocunha@alu.ufc.br>
2024-11-25 15:52:39 -0300

    att

--- POS-JAVA-Netbeans-MySql-master/pos pro/src/pos/pro/db.java
+++ POS-JAVA-Netbeans-MySql-master/pos pro/src/pos/pro/db.java
@@ -1,42 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pos.pro;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author Go
- */
 public class db {
-   
- public static Connection mycon(){
- 
- 
-     Connection con = null ;
-     
-     try {
-         
-         Class.forName("com.mysql.jdbc.Driver");
-         con = DriverManager.getConnection("jdbc:mysql://localhost/pos","root","");
-         return con;
-         
-         
-     } catch (ClassNotFoundException | SQLException e) {
-         
-         System.out.println(e);
-         return null;
-     }
-     
- 
- 
- }   
-    
-    
-    
+    public static Connection mycon() {
+        Connection con = null;
+        try {
+            // Carregar o driver JDBC para MySQL
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // URL de conexão com o banco de dados
+            String url = "jdbc:mysql://localhost:3306/pos"; // Altere "pos" para o nome do seu banco de dados
+            String user = "root"; // Altere para seu usuário
+            String password = "12345"; // Altere para sua senha
+
+            // Estabelecer a conexão
+            con = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexão estabelecida com sucesso!");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver não encontrado: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+        }
+        return con;
+    }
 }
